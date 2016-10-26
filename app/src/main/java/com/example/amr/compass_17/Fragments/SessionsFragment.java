3250a18cdd2466826df9cde4e0ecbadae3e93650
AhmedHassaan <ahmedhassaan162@gmail.com>
2016-10-27 01:10:11 +0200

    Workshop added

--- app/src/main/java/com/example/amr/compass_17/Fragments/SessionsFragment.java
+++ app/src/main/java/com/example/amr/compass_17/Fragments/SessionsFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.amr.compass_17.LstViewAdapter;
 import com.example.amr.compass_17.R;
 
 /**
@@ -16,6 +19,20 @@ public class SessionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.sessions_fragment,container,false);
+
+
+        ListView lstview=(ListView)view.findViewById(R.id.listview);
+        // Inflate header view
+        ViewGroup headerView = (ViewGroup)getLayoutInflater(null).inflate(R.layout.header, lstview,false);
+        // Add header view to the ListView
+        lstview.addHeaderView(headerView);
+        // Get the string array defined in strings.xml file
+        String[] items=getResources().getStringArray(R.array.list_items);
+        // Create an adapter to bind data to the ListView
+        LstViewAdapter adapter=new LstViewAdapter(getActivity(),R.layout.rowlayout,R.id.txtname,items);
+        // Bind data to the ListView
+        lstview.setAdapter(adapter);
+
 
         return view;
     }
---
+++ app/src/main/java/com/example/amr/compass_17/LstViewAdapter.java
@@ -0,0 +1,55 @@
+package com.example.amr.compass_17;
+
+/**
+ * Created by Lenovo on 10/26/2016.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+public class LstViewAdapter extends ArrayAdapter<String> {
+    int groupid;
+    String[] item_list;
+    ArrayList<String> desc;
+    Context context;
+    public LstViewAdapter(Context context, int vg, int id, String[] item_list){
+        super(context,vg, id, item_list);
+        this.context=context;
+        groupid=vg;
+        this.item_list=item_list;
+
+    }
+    // Hold views of the ListView to improve its scrolling performance
+    static class ViewHolder {
+        public TextView textname;
+        public TextView texttime;
+
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View rowView = convertView;
+        // Inflate the rowlayout.xml file if convertView is null
+        if(rowView==null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView= inflater.inflate(groupid, parent, false);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.textname= (TextView) rowView.findViewById(R.id.txtname);
+            viewHolder.texttime= (TextView) rowView.findViewById(R.id.txttime);
+            rowView.setTag(viewHolder);
+
+        }
+        // Set text to each TextView of ListView item
+        String[] items=item_list[position].split("__");
+        ViewHolder holder = (ViewHolder) rowView.getTag();
+        holder.textname.setText(items[0]);
+        holder.texttime.setText(items[1]);
+        return rowView;
+    }
+
+}
