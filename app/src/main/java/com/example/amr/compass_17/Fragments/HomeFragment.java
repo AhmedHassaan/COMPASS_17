71e63e28a7180f04bda03be3bc9b6ecc37188749
Abdelrahman <abdelrahman.mosa3d.fcis@hotmail.com>
2017-02-17 14:29:33 +0200

    New Home Fragment - Abdelrahman's Task

--- app/src/main/java/com/example/amr/compass_17/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/amr/compass_17/Fragments/HomeFragment.java
@@ -3,6 +3,8 @@ package com.example.amr.compass_17.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,8 +23,7 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
     NewsAdapter newsAdapter;
-    //RecyclerView recyclerView;
-    ExpandableListView expandableListView;
+    RecyclerView recyclerView;
     List<News> newsList = new ArrayList<>();
 
     public HomeFragment(){}
@@ -34,32 +35,20 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment,container,false);
-        //recyclerView = (RecyclerView)view.findViewById(R.id.homePage_recyclerView);
-        expandableListView = (ExpandableListView)view.findViewById(R.id.homePage_list);
+        recyclerView = (RecyclerView) view.findViewById(R.id.news_recyclerview);
 
-        News news = new News("Hi",R.drawable.ic_launcher,"hello");
-        newsList.add(news);
-        newsList.add(news);
-        newsList.add(news);
-        news = new News("Welcome",R.drawable.ic_launcher
-                ,"ay kalam bas al mohem tasht3'l ya3ny w al article yb2a latef w rbna ya5lek l om al donya w mtnsash al fakka yastaa");
-        newsList.add(news);
-        newsList.add(news);
-        newsList.add(news);
-        newsList.add(news);
-        newsList.add(news);
-        newsList.add(news);
-        newsList.add(news);
-        newsList.add(news);
-        newsList.add(news);
+        newsList = new ArrayList<>();
+        newsList.add(new News("Antalya","http://res.cloudinary.com/dflpifew9/image/upload/v1486849057/antalya_hg4npr.jpg","This is a test article to see if it works correctly or not."));
+        newsList.add(new News("Turkey","http://res.cloudinary.com/dflpifew9/image/upload/v1486849105/turkey_lbr9iv.jpg","This is a test article to see if it works correctly or not."));
+        newsList.add(new News("Antalya","http://res.cloudinary.com/dflpifew9/image/upload/v1486849057/antalya_hg4npr.jpg","This is a test article to see if it works correctly or not."));
+        newsList.add(new News("Turkey","http://res.cloudinary.com/dflpifew9/image/upload/v1486849105/turkey_lbr9iv.jpg","This is a test article to see if it works correctly or not."));
+        newsAdapter = new NewsAdapter(getContext(),newsList);
 
-        newsAdapter = new NewsAdapter(newsList);
-        //RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        //recyclerView.setLayoutManager(layoutManager);
-        //recyclerView.addItemDecoration(new VerticalSpaceItemDecoration(48));
-        //recyclerView.setAdapter(newsAdapter);
-        expandableListView.setAdapter(newsAdapter);
-        expandableListView.setGroupIndicator(null);
+
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(newsAdapter);
 
         return view;
     }
---
+++ app/src/main/java/com/example/amr/compass_17/NewsDetailActivity.java
@@ -0,0 +1,49 @@
+package com.example.amr.compass_17;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.transition.Explode;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class NewsDetailActivity extends AppCompatActivity {
+
+    TextView title,article;
+    ImageView image;
+    //CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.myCollapsingToolbar);
+        //collapsingToolbarLayout.setTitle("COMPASS 17");
+
+
+        title = (TextView)findViewById(R.id.detail_title_textview);
+        article = (TextView)findViewById(R.id.detail_article_textview);
+        image = (ImageView)findViewById(R.id.detail_image_textview);
+
+        Intent intent = getIntent();
+        title.setText(intent.getStringExtra("title"));
+        article.setText(intent.getStringExtra("article"));
+
+        Picasso.with(this)
+                .load(intent.getStringExtra("image")).into(image);
+
+
+        Explode explode = new Explode();
+        explode.setDuration(750);
+        getWindow().setEnterTransition(explode);
+    }
+}
